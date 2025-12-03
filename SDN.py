301b376982b8e0c55fd6851d2909c2e5516b44af
DhritiAgrawal13 <dhritiagrawal13@gmail.com>
2025-12-03 21:19:06 +0530

    Update SDN.py

--- SDN.py
+++ SDN.py
@@ -6,10 +6,11 @@ import json
 from google import genai
 import os 
 import gdown
-
+from dotenv import load_dotenv as env 
+env()
 
 # os.environ["GOOGLE_API_KEY"]  = "AIzaSyDfkkMquLJv_BZXlqyYkAHfkRloR6-y1mw"
-a=st.secrets('GeminiAPi')
+a=os.getenv('GeminiAPi')
 client = genai.Client(api_key=a)
 
 
